4b313bce97389ad36f69ce4ebb1a557839d73b7d
MukundVisavadiya <131982370+MukundVisavadiya@users.noreply.github.com>
2026-04-10 18:07:00 +0530

    Responsive

--- pages/api/postcontact.js
+++ pages/api/postcontact.js
@@ -3,7 +3,6 @@ import * as fs from "fs";
 export default async function handler(req, res) {
   if (req.method === "POST") {
     let data = await fs.promises.readdir("contactdata");
-    console.log(data);
     fs.promises.writeFile(
       `contactdata/${data.length + 1}.json`,
       JSON.stringify(req.body),
